43c33134e2b69b134a60ae92b5b1d33ac965ce9f
Bivil M Jacob <bivilj@gmail.com>
2018-05-01 16:16:28 +0530

    Admin Role and validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:edit, :update, :show, :destroy]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destory]
 
   def new
     @user = User.new()
@@ -10,8 +11,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "Welcome to the Beta blog #{@user.name}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
@@ -37,6 +39,13 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 3)
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles has been deleted"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:name,:email,:password)
@@ -47,8 +56,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "You can edit only your profile "
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and current_user.admi?
+      flash[:danger] = "Only admin can delete User"
       redirect_to root_path
     end
   end
